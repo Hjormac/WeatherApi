a8f49833d058742ddbf78e909668d4048107abad
Hjormac <t8romi00@students.oamk.fi>
2019-12-03 15:23:59 +0200

    first

---
+++ models/Wdata.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+let Wdata = new Schema({
+  _id: {
+    type: Number
+  },
+  temperature: {
+    type: Number
+  },
+  humidity: {
+    type: Number
+  },
+  time:{
+    type: String
+  },
+  },{
+    collection: 'Data', versionKey :false
+  }
+  );
+
+module.exports = mongoose.model('Wdata', Wdata);
